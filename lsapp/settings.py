168083b519c41349c4217fc9cb8b0050a515869f
Shaji SN <shaji@AUJAN_DOMAIN.COM>
2023-01-23 12:48:49 +0300

    complete teams model

--- lsapp/settings.py
+++ lsapp/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'lsapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lsapp',
+        'USER': 'postgres',
+        'PASSWORD': 'sqlm@n123',
+        'HOST': 'localhost',
     }
 }
 
@@ -121,6 +124,11 @@ STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     BASE_DIR, 'lsapp/static',
 ]
+
+#Media settings
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
